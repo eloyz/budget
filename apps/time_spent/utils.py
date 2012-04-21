49c3c7c7687d63b4bfe9a60adc036cc2a7a9d88a
Eloy Zuniga Jr <eloyz.email@gmail.com>
2012-04-21 02:19:56 -0500

    Moved list of "expense colors" to tuple in utils file. Ideally I would generate this list of colors in the future.

---
+++ apps/time_spent/utils.py
@@ -0,0 +1,16 @@
+
+
+# Warm to cool colors
+# Ideally this list could be generated
+EXPENSE_COLORS = (
+	'#AB2F27', # red
+	'#D08021', # orange
+	'#D19919', # yelloworange
+	'#EFC443', # yellow
+	'#A7B929', # olive
+	'#84BB3B', # lime
+	'#57A336', # green
+	'#45A388', # bluegreen
+	'#4784A3', # teal
+	'#668ADE', # blue
+)
