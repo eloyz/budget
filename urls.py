02840b196ecaf05dedb474cd4f55075e597899a7
Eloy Zuniga Jr <eloyz.email@gmail.com>
2012-04-21 02:23:49 -0500

    Removing code for urls.py file. Explicitly referencing modules instead of implicitly requesting all modules from a django urls.

--- urls.py
+++ urls.py
@@ -1,9 +1,8 @@
 import os
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url, include
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,15 +10,10 @@ urlpatterns = patterns('',
 	(r'^$', include('homepage.urls')),
 	(r'^time-spent/', include('time_spent.urls')),
 	(r'^accounts/', include('registration.urls')),
-
-    # Example:
-    # (r'^Fudget/', include('Fudget.foo.urls')),
-
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
 
-# Local url patterns for development
 try:
     from local_urls import extra_patterns
     urlpatterns += extra_patterns
