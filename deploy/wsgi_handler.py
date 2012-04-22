eb3a0c91e296afb61f695ff68f88d38085f24dea
Eloy Zuniga Jr <eloyz.email@gmail.com>
2012-04-22 08:50:52 -0500

    Updating wsgi_handler with new project name.  Also changing to more secure permissions.

--- deploy/wsgi_handler.py
+++ deploy/wsgi_handler.py
@@ -12,7 +12,7 @@ sys.path.insert(0, abspath(join(dirname(__file__), "../../")))
 
 from django.conf import settings
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "fudget.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "budget.settings"
 sys.path.insert(0, join(settings.PROJECT_ROOT))
 
 from django.core.handlers.wsgi import WSGIHandler
