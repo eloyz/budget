29116e7a78e0c4237842e847e012eddf8ae5f77c
Eloy Zuniga Jr <eloyz.email@gmail.com>
2012-04-22 07:44:49 -0500

    Removing commented out template loader.

--- settings.py
+++ settings.py
@@ -65,7 +65,6 @@ SECRET_KEY = ''
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
