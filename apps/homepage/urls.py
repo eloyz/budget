16cab0a1827721f81814df1605b6425013da6fe3
Eloy Zuniga Jr <eloyz.email@gmail.com>
2012-05-24 00:29:15 -0500

    Remove unused code.

--- apps/homepage/urls.py
+++ apps/homepage/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('homepage.views', 
+urlpatterns = patterns('homepage.views',
     url(r'^$', 'details', name="homepage"),
 )
